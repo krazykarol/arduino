330aff196a87e06974994fd9bbb90224ca8d2eaf
makarol <manikkarol@outlook.com>
2020-07-03 19:28:04 +0530

    All working independently and calibrating

---
+++ projects/4x4Robot/Intelligent_4x4_Robot/PinConfig.h
@@ -0,0 +1,27 @@
+#ifndef _PIN_CONFIG_
+#define _PIN_CONFIG_
+
+/***
+   START PIN CONFIGURATIONS
+*/
+
+/** DHT **/
+#define DHT22_PIN A5                   // Pin for the DHT22 Sensor output
+
+/*** Drive Train LED ***/
+#define RED_PIN A3                    // Pulse the RED PIN for LED
+#define GREEN_PIN A4                  // Pulse the GREEN PIN for LED
+
+/*** Sensor PLate ***/
+#define SERVO_PIN 10                  // define the SERVO Digital PIN
+
+/*** SONAR SENSOR PINS ***/
+#define ECHO_PIN A8                   // define the Echo pin of For UltraSonic Distance sensor;
+#define TRIGGER_PIN A9                // define the Trigger pin of For UltraSonic Distance sensor;
+
+
+/**
+   END PIN CONFIGURATIONS
+*/
+
+#endif
